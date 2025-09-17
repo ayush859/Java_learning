2b86cceb70b7ca20db991e5fb6755e7e5499effb
ayush <ayushgtg@outlook.com>
2025-09-18 01:03:14 +0530

    updated Pattern 7 code

--- 08_09_Language_Basics/03_PATTERNS/_07_Pattern.java
+++ 08_09_Language_Basics/03_PATTERNS/_07_Pattern.java
@@ -11,7 +11,7 @@ class Solution {
 
         for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j < n-i; j++) {
+            for (int j = 0; j < n-i; j++) {
                 System.out.print(" ");
             }
 
